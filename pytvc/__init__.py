fc25a76c86e74e8a88e54abfdee5382bb2f90453
Daniel J. Umpierrez <umpierrez@pm.me>
2018-11-09 16:00:28 +0100

    README filename fix and version update.

--- pytvc/__init__.py
+++ pytvc/__init__.py
@@ -11,22 +11,22 @@
 import os
 import sys
 
+from pytvc.core import TradingViewChart
+
 sys.path.append(os.path.dirname(os.path.basename(__file__)))
 sys.path.append(os.path.dirname(__file__))
-
-
-from pytvc.core import TradingViewChart
 
 __project__ = 'PyTVC'
 __package__ = 'pytvc'
 __author__ = 'Daniel J. Umpierrez'
 __license__ = 'UNLICENSE'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __description__ = __doc__
 __site__ = 'https://github.com/havocesp/{}'.format(__package__)
 __email__ = 'umpierrez@pm.me'
-__long_description__ = open('../README.me').read()
+__long_description__ = open('../README.md').read()
 __keywords__ = ['altcoins', 'altcoin', 'exchange', 'pandas', 'bitcoin', 'trading', 'tradingview', 'chart', 'finance']
 __dependencies__ = ['ccxt', 'begins']
+
 __all__ = ['__description__', '__author__', '__license__', '__version__', '__project__', '__site__',
            '__email__', '__keywords__', 'TradingViewChart']
