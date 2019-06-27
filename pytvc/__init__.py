b5635ab6ba97b2132f2c2ce75cebe727c509d9e1
Daniel J. Umpierrez <umpierrez@pm.me>
2019-06-28 01:57:27 +0200

    Readme file content read moved to setup file.

--- pytvc/__init__.py
+++ pytvc/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
- PyTVC
+"""PyTVC
 
  Python TradingView Chart
 
@@ -8,11 +7,7 @@
  - Created:     03-11-2018
  - License:     UNLICENSE
 """
-import pathlib
-
 from pytvc.core import TradingViewChart
-
-_ROOT = pathlib.Path(__file__).parent.parent  # type: pathlib.Path
 
 __project__ = 'PyTVC'
 __package__ = 'pytvc'
@@ -22,7 +17,6 @@ __version__ = '0.1.3'
 __description__ = __doc__
 __site__ = f'https://github.com/havocesp/{__package__}'
 __email__ = 'umpierrez@pm.me'
-__long_description__ = _ROOT.joinpath('README.md').read_text()
 __keywords__ = ['altcoins', 'altcoin', 'exchange', 'pandas', 'bitcoin', 'trading', 'tradingview', 'chart', 'finance']
 __dependencies__ = ['ccxt', 'begins']
 
