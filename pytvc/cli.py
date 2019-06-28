1de8a19e1f6117637ae33a6be5a9fffdbd4b2274
Daniel J. Umpierrez <umpierrez@pm.me>
2019-06-28 03:06:08 +0200

    CLI args changed and fixed some errors.

--- pytvc/cli.py
+++ pytvc/cli.py
@@ -19,7 +19,7 @@ def load_indicators():
 
     :return:
     """
-    ti = dict()
+    ti = list()
     try:
         ti_file: Path = Path(__file__).with_name('json').joinpath('tv_indicators.json')
         if ti_file.exists() and ti_file.is_file():
@@ -27,16 +27,17 @@ def load_indicators():
             ti = json.loads(text)
     except (TypeError, ValueError, IOError,) as err:
         print(str(err))
+        raise err
     return ti 
 
 
-def list_indicators(*args, **kwargs) -> int:
+def list_indicators() -> int:
     """List all supported indicators.
 
     :return: 0 if all was fine.
     """
     ti = load_indicators()
-    indicators = [f'- {v:<30}\n' for v in ti]
+    indicators = [f'- {v:<30}' for v in ti]
     indicators.sort()
     for i in indicators:
         print(i)
@@ -44,7 +45,7 @@ def list_indicators(*args, **kwargs) -> int:
 
 
 def main(args) -> int:
-    """TradingView Chart launcher.
+    """TradingView Chart parser.r.
     
     :param args:
     :return:
@@ -55,68 +56,54 @@ def main(args) -> int:
 
 
 def run() -> int:
-    """ As CLI starting point, this function dispatch argument parsing to be supplied to main function.
+    """As CLI starting point, this function dispatch argument parsing to be supplied to main function.
 
     :return: a 0 value for execution success or a non zero value containing the clue about where to find the error :-).
-    :rtype: int
     """
-    # exit code
-    r_code = 0
-
     base_markets = ['BTC', 'TUSD', 'USDT', 'USD', 'EUR', 'PAX', 'USDS']
-    exchanges = ['binance', 'cryptopia', 'hitbtc2', 'poloniex', 'kraken', 'coinbase', 'cexio']
+    exchanges = ['binance', 'hitbtc2', 'poloniex', 'kraken', 'coinbase', 'cexio']
     exchanges = {e: e.strip('_12345 ') for e in exchanges}
 
-    load_indicators()
+    parser = argparse.ArgumentParser()                                                                                                                                   
+                                                                                                                                                                         
+    parser.add_argument('-l, --list-indicators',                                                                                                                         
+                        dest='list_indicators',                                                                                                                          
+                        help='List all supported technical indicators')                                                                                                  
+                                                                                                                                                                         
+    parser.add_argument('-e, --exchange',                                                                                                                                
+                        default='binance',                                                                                                                               
+                        dest='exchange',                                                                                                                                 
+                        nargs='?',                                                                                                                                       
+                        choices=exchanges,                                                                                                                               
+                        help='Exchange used for watch list symbols.')                                                                                                    
+                                                                                                                                                                         
+    parser.add_argument('-q', '--quote-currency',                                                                                                                        
+                        nargs='?',                                                                                                                                       
+                        default='BTC',                                                                                                                                   
+                        choices=base_markets,                                                                                                                            
+                        help='Base market currency (default BTC)')                                                                                                       
+                                                                                                                                                                         
+    parser.add_argument('-i', '--indicator',                                                                                                                             
+                        metavar='TI',                                                                                                                                    
+                        nargs='*',                                                                                                                                       
+                        choices=list(load_indicators().keys()),                                                                                                          
+                        help='Technical analysis indicators to be showed within chart.')                                                                                 
+                                                                                                                                                                         
+    parser.add_argument('-m', '--min-volume',                                                                                                                            
+                        type=float,                                                                                                                                      
+                        nargs='?',                                                                                                                                       
+                        dest='min_volume',                                                                                                                               
+                        default=0.0,                                                                                                                                     
+                        help='Min. 24h volume filter.')                                                                                                                  
 
-    parser = argparse.ArgumentParser()
-
-    subparsers = parser.add_subparsers(description='Commands')
-
-    launch = subparsers.add_parser('launch', help='a help')
-
-    launch.add_argument('-e, --exchange',
-                        default='binance',
-                        dest='exchange',
-                        nargs='?',
-                        choices=exchanges,
-                        help='Exchange used for watch list symbols.')
-
-    launch.add_argument('-q', '--quote-currency',
-                        nargs='?',
-                        default='BTC',
-                        choices=base_markets,
-                        help='Base market currency (default BTC)')
-
-    launch.add_argument('-i', '--indicator',
-                        metavar='TI',
-                        nargs='*',
-                        choices=list(load_indicators().keys()),
-                        help='Technical analysis indicators to be showed within chart.')
-
-    launch.add_argument('-m', '--min-volume',
-                        type=float,
-                        nargs='?',
-                        dest='min_volume',
-                        default=0.0,
-                        help='Min. 24h volume filter.')
-
-    launch.set_defaults(func=main)
-
-    indicators = subparsers.add_parser('list-indicators',
-                                       help='List all supported technical indicators.')
-
-    indicators.set_defaults(func=list_indicators)
-
-    args = parser.parse_args()
-
-    if hasattr(args, 'func'):
-        r_code = args.func(args)
+    args = parser.parse_args()                                                        
+    
+    if args.list_indicators is True:
+        r_code = list_indicators()
     else:
-        parser.print_help()
+        r_code =  main(args)
 
-    return r_code
+    sys.exit(r_code)
 
-
-if __name__ == '__main__':
-    sys.exit(run())
+if __name__ == '__main__': 
+    run()
